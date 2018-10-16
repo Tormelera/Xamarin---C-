2a88b058be3c2aeaf7b0273c8c0d4841e9225c93
Auvo <arturbarbosati@gmail.com>
2018-10-16 17:08:19 -0300

    Commit com BancoDeDados, sem edição

--- Auvo_Project/Auvo_Project/Auvo_Project/AcessoDados.cs
+++ Auvo_Project/Auvo_Project/Auvo_Project/AcessoDados.cs
@@ -3,36 +3,12 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using Xamarin.Forms;
+using SQLite;
 
 namespace Auvo_Project
 {                           //Estudar IDisposable
-    public class AcessoDados // :IDisposable
+    public class AcessoDados 
     {
-        /*
-        private SQLite.Net.SQLiteConnection _conexao;
-
-        public AcessoDados()
-        {
-            var config = DependencyService.Get<Auvo_Project.Droid.IConfig>();
-            _conexao = new SQLite.Net.SQLiteConnection(config.Plataforma, System.IO.Path.Combine(config.DiretorioDB, "bancodados.db3"));
-
-            _conexao.CreateTable<Usuario>();
-        }
-
-        public void Insert(Usuario usuario)
-        {
-            _conexao.Insert(usuario);
-        }
-
-        public void Delete(Usuario usuario)
-        {
-            _conexao.Delete(usuario);
-        }
-
-        public void Dispose()
-        {
-            
-        }
-        */
+        
     }
 }
--- Auvo_Project/Auvo_Project/Auvo_Project/ViewModels/MainViewModel.cs
+++ Auvo_Project/Auvo_Project/Auvo_Project/ViewModels/MainViewModel.cs
@@ -5,95 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Auvo_Project;
+using Auvo_Project.Model;
 
 namespace Auvo_Project.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+    public class MainViewModel : Pessoa
     {
-        /*
-        private string _nome;
-
-        public string Nome
-        {
-            get { return _nome; }
-            set
-            {
-                if (SetProperty(ref _nome, value))
-                    SaveCommand.ChangeCanExecute();
-                
-            }
-        }
-
-        private string _cpf;
-
-        public string CPF
-        {
-            get { return _cpf; }
-            set
-            {
-                if (SetProperty(ref _cpf, value))
-                    SaveCommand.ChangeCanExecute();
-            }
-        }
-
-        private string _email;
-
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                if (SetProperty(ref _email, value))
-                    SaveCommand.ChangeCanExecute();
-            }
-        }
-
-        private string _login;
-
-        public string Login
-        {
-            get { return _login; }
-            set
-            {
-                if (SetProperty(ref _login, value))
-                    SaveCommand.ChangeCanExecute();
-            }
-        }
-
-        private string _senha;
-
-        public string Senha
-        {
-            get { return _senha; }
-            set
-            {
-                if (SetProperty(ref _senha, value))
-                    SaveCommand.ChangeCanExecute();
-            }
-        }
-
-        
-        */
-
-
         async void ExecuteSaveCommand()
         {
 
             await Task.Delay(1500);
 
-            Nome = this.Nome;
-            CPF = this.CPF;
-            Email = this.Email;
-            Login = this.Login;
-            Senha = this.Senha;
-
             await App.Current.MainPage.DisplayAlert("Auvo_Project", $"Confirma os dados abaixo?\nNome: '{Nome}';\n" +
                 $"CPF: '{CPF}';\n" +
                 $"E-mail: '{Email}';\n" +
                 $"Usuário: '{Login}'.", "Sim", "Não");
-
-            
-
 
         }
 
